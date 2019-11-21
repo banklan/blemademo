32a073b2608300e77b4ed7d077d47a68daf06ece
banky <banklan2010@gmail.com>
2019-11-21 21:02:23 +0100

    fixed db.php

--- config/database.php
+++ config/database.php
@@ -82,10 +82,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'engine' => null
         ],
 
         'pgsql' => [
