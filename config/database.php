6209f56e12d05acdb5ebfe776da20cae8e6f9305
banky <banklan2010@gmail.com>
2019-11-23 01:08:07 +0100

    database.php updated2

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-    // $url = parse_url(getenv("DATABASE_URL"));
+    var_dump(parse_url(getenv("DATABASE_URL")));
     // $host = $url['host']?? null;
     // $username = $url['user'] ?? null;
     // $password = $url['pass'] ?? null;
