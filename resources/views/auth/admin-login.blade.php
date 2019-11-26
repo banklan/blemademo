7db71d71354a3e1147059933bde0ec655d8dcfeb
banky <banklan2010@gmail.com>
2019-11-26 23:28:48 +0100

    card fixed

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.navbar')
+    @include('layouts.admin_navbar')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
