77640c551e25f1609c9806ebad38e63f517b72df
banky <banklan2010@gmail.com>
2019-11-28 19:37:00 +0100

    fix s3 path4

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
         Barryvdh\Cors\ServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Unicodeveloper\Paystack\PaystackServiceProvider::class,
-        Aws\Laravel\AwsServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -232,7 +231,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Paystack' => Unicodeveloper\Paystack\Facades\Paystack::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'AWS' => Aws\Laravel\AwsFacade::class,
 
     ],
 
